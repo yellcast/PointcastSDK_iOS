6691bac903749b250243375ef4ff906669af3692
Yellcast <37024455+appsmall@users.noreply.github.com>
2022-05-13 21:26:40 +0530

    first commit 0.0.1

---
+++ README.md
@@ -0,0 +1,3 @@
+# PointcastSDKPackage
+
+A description of this package.
---
+++ Sources/PointcastSDK.xcframework/ios-arm64/PointcastSDK.framework/Headers/PointcastSDK.h
@@ -0,0 +1,18 @@
+//
+//  PointcastSDK.h
+//  PointcastSDK
+//
+//  Created by Rahul Chopra on 13/05/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PointcastSDK.
+FOUNDATION_EXPORT double PointcastSDKVersionNumber;
+
+//! Project version string for PointcastSDK.
+FOUNDATION_EXPORT const unsigned char PointcastSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PointcastSDK/PublicHeader.h>
+
+
